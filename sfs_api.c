bd23c773475224ba20428c382d240992ef8329da
arunench <arunench@gmail.com>
2017-03-25 16:53:29 -0400

    Adding all necessary c and header files

---
+++ sfs_api.c
@@ -0,0 +1,32 @@
+#include "sfs_api.h"
+
+void mkssfs(int fresh){
+
+}
+int ssfs_get_next_file_name(char *fname){
+    return 0;
+}
+int ssfs_get_file_size(char* path){
+    return 0;
+}
+int ssfs_fopen(char *name){
+    return 0;
+}
+int ssfs_fclose(int fileID){
+    return 0;
+}
+int ssfs_frseek(int fileID, int loc){
+    return 0;
+}
+int ssfs_fwseek(int fileID, int loc){
+    return 0;
+}
+int ssfs_fwrite(int fileID, char *buf, int length){
+    return 0;
+}
+int ssfs_fread(int fileID, char *buf, int length){
+    return 0;
+}
+int ssfs_remove(char *file){
+    return 0;
+}
