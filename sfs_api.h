73ea2f4444a1f12710ed12cce1fe352c671c5f11
arunench <arunench@gmail.com>
2017-03-25 21:47:54 -0400

    Added necessary global macros and structs

--- sfs_api.h
+++ sfs_api.h
@@ -1,3 +1,63 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include "disk_emu.h"
+
+#define SSFS_MAX_FILENAME 16
+#define SSFS_BLOCK_SIZE 1024
+#define SSFS_MAGIC_NUM 4
+#define SSFS_FD_ENTRIES 1024
+
+typedef struct{
+	unsigned char magic[SSFS_MAGIC_NUM];
+	int block_size;
+	int fs_size;
+	int root_inode_num;
+	int inode_table_len;
+} superblock_t;
+
+typedef struct{
+	int size;
+	int direct[14];
+	int indirect;
+	int mode;
+} inode_t;
+
+typedef struct{
+	uint8_t unsigned char bytes [SSFS_BLOCK_SIZE];
+} block_t;
+
+typedef struct{
+	int size;
+	int	inodes_used;
+	char *inodes_unused;
+	inode_t *inode;
+}	inode_table;
+
+typedef struct{
+	int size;
+	fd_entry entry[SSFS_FD_ENTRIES];
+} fd_table;
+
+typedef struct{
+	int size;
+	fd_entry entry[SSFS_FD_ENTRIES];
+	int inode_num;
+	int read_ptr;
+	int write_ptr;
+} fd_entry;
+
+typedef struct{
+	directory_entry *entry;
+	int num_directories; 
+} directory_table;
+
+typedef struct{
+	int inode_num;
+	char filename[SSFS_MAX_FILENAME];
+} directory_table;
+
 //Functions you should implement. 
 //Return -1 for error besides mkssfs
 void mkssfs(int fresh);
